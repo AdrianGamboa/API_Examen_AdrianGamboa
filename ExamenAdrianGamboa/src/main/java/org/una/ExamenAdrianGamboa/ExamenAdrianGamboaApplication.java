273621052e5fd8a4364a7863c8875f1aac93eb7c
Norman Adrian Gamboa Delgado <70042309+AdrianGamboa@users.noreply.github.com>
2020-10-26 20:44:41 -0600

    Creación del proyecto

---
+++ ExamenAdrianGamboa/src/main/java/org/una/ExamenAdrianGamboa/ExamenAdrianGamboaApplication.java
@@ -0,0 +1,13 @@
+package org.una.ExamenAdrianGamboa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenAdrianGamboaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenAdrianGamboaApplication.class, args);
+	}
+
+}
---
+++ ExamenAdrianGamboa/src/test/java/org/una/ExamenAdrianGamboa/ExamenAdrianGamboaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.ExamenAdrianGamboa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAdrianGamboaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
